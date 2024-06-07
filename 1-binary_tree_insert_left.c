0894954543c015badc2974b79f0ab692692f1b4d
ZAKARIA TOURAS <zakariatouras47@gmail.com>
2024-06-06 19:23:11 -0700

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,19 +7,17 @@
  * @value: the value to store in the new node
  * Return: a pointer to the new node or NULL on failure
  */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
+
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 	if (parent->right)
 	{
 		new_node->right = parent->right;
